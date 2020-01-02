ce5198a0b9127ae8c944f68a1cabe8f22565be40
Steven Frasica <sfrasica1@gmail.com>
2020-01-02 00:58:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,12 @@ def long_planeteer_calls(planeteer_calls)
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
