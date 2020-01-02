5630e0aebe9c068d807b0bdebb1e27473aa394d6
Steven Frasica <sfrasica1@gmail.com>
2020-01-02 00:50:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,11 +13,22 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map { |call| call.capitalize + '!' }# Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(planeteer_calls)# code an argument here
+  planeteer_calls.any? { |call| call.length < 4 }# Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+long_planeteer_calls
+
+potentially_cheesy_items = %w[umbrella spinach chedder helicopter]
+
+def find_the_cheese(potentially_cheesy_items)# code an argument here
+  cheese = %w[gouda cheddar camembert]# the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
+
+find_the_cheese(potentially_cheesy_items)
