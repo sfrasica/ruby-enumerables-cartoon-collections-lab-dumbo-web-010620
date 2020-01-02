113087abff30e6be3b052d748462e9a3b78c380e
Steven Frasica <sfrasica1@gmail.com>
2020-01-02 00:52:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(planeteer_calls)# code an argument here
-  planeteer_calls.any? { |call| call.length < 4 }# Your code here
+  planeteer_calls.any? { |call| call.length > 4 }# Your code here
 end
 
 long_planeteer_calls
